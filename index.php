9ef5185964261912815d985f486adb41fc893d94
Noah <59740042+noahjeffers@users.noreply.github.com>
2020-03-12 09:31:24 -0500

    step 1

--- index.php
+++ index.php
@@ -1,5 +1,62 @@
 <?php
 
-TEST TEST TEST
 
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <div class="header">
+      <h2>NETWORK NAME</h2>
+    </div>
+    <div class="sidebar">
+      <ul class="Creators">
+        <li>
+          <a href="#">Creator 1</a>
+        </li>
+        <li>
+          <a href="#">Creator 2</a>
+        </li>
+        <li>
+          <a href="#">Creator 3</a>
+        </li>
+        <li>
+          <a href="#">Creator 4</a>
+        </li>
+        <li>
+          <a href="#">Creator 5</a>
+        </li>
+        <li>
+          <a href="#">Creator 6</a>
+        </li>
+      </ul>
+    </div>
+    <div class="body">
+      <div class="nav">
+        <ul>
+        <li>
+          <a href="#">Comedy</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">History</a>
+        </li>
+        <li>
+          <a href="#">Horror</a>
+        </li>
+        <li>
+          <a href="#">Crime</a>
+        </li>
+        </ul>
+      </div>
+
+    </div>
+    </header>
+  </body>
+</html>
